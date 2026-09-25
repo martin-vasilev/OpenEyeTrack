acc6269f06eb39eebac4422384e6b74a4ba80849
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2026-09-25 21:22:00 +0100

    Expose shared-frame shadow diagnostics in samples

--- src/types/Sample.ts
+++ src/types/Sample.ts
@@ -1,40 +1,13 @@
 export interface EyeTrackingSample {
   timestamp:number; frameId:number;
-  gazeXRaw:number|null; gazeYRaw:number|null; gazeX:number|null; gazeY:number|null;
-  gazeConfidence:number|null; gazeOutlier:boolean|null; gazeRawDeviationPx:number|null;
-  pupilLeft:number|null; pupilRight:number|null;
-  headX:number|null; headY:number|null; headZ:number|null; headYaw:number|null; headPitch:number|null; headRoll:number|null;
-  headPoseDistanceFromCalibration:number|null;
-  eyeConfidence:number|null;
-  elgLeftRelX:number|null; elgLeftRelY:number|null; elgRightRelX:number|null; elgRightRelY:number|null;
-  elgLeftConfidence:number|null; elgRightConfidence:number|null;
-  elgLeftRelXRaw:number|null; elgLeftRelYRaw:number|null; elgRightRelXRaw:number|null; elgRightRelYRaw:number|null;
-  elgBinocularReliability:number|null;
-  trial:string|null; event:string|null; calibrationId:string|null; calibrationPoints:number|null;
-  calibrationRepetitions:number|null; calibrationSettleMs:number|null; calibrationSampleMs:number|null;
-  calibrationRandomized:boolean|null; calibrationObservations:number|null;
-  validationMeanPx:number|null; validationMedianPx:number|null; validationRmsePx:number|null;
-  validationPrecisionRmsS2SPx:number|null; validationPrecisionSdPx:number|null; validationDataLoss:number|null;
-  validationValidPoints:number|null;
-  calibrationHeadReferenceSamples:number|null;
-  calibrationHeadX:number|null; calibrationHeadXSd:number|null; calibrationHeadY:number|null; calibrationHeadYSd:number|null;
-  calibrationHeadZ:number|null; calibrationHeadZSd:number|null; calibrationHeadYaw:number|null; calibrationHeadYawSd:number|null;
-  calibrationHeadPitch:number|null; calibrationHeadPitchSd:number|null; calibrationHeadRoll:number|null; calibrationHeadRollSd:number|null;
-
-  // Optional developer diagnostics. These keys are omitted entirely from normal recordings.
-  cameraTrackFps?:number|null; videoCallbackIntervalMs?:number|null; videoPresentedFrameDelta?:number|null; videoMissedPresentedFramesTotal?:number;
-  pipelineTotalMs?:number|null; mediaPipeDispatchToBitmapMs?:number|null; mediaPipeBitmapToPostMs?:number|null; mediaPipeWorkerQueueMs?:number|null;
-  mediaPipeWorkerInferenceMs?:number|null; mediaPipeWorkerReturnMs?:number|null; mediaPipeEndToEndMs?:number|null; mediaPipeResultAgeMs?:number|null; mediaPipeSequenceId?:number|null;
-  elgEnqueueMs?:number|null; featureExtractionMs?:number|null; gazePredictionMs?:number|null; overlayRenderMs?:number|null; elgAcquisitionMode?:string|null;
-  elgInferenceMs?:number|null; elgAgeMs?:number|null; elgSequenceId?:number|null; elgAcquisitionTimestampMs?:number|null; elgProcessedTimestampMs?:number|null;
-  elgLatencyMs?:number|null; elgQueueDepth?:number|null; elgCropPreprocessMs?:number|null; elgQueueWaitMs?:number|null; elgTensorSetupMs?:number|null; elgOnnxInferenceMs?:number|null; elgDecodeMs?:number|null;
-  elgFirstEye?:"left"|"right"|null;
-  elgLeftCropTotalMs?:number|null; elgRightCropTotalMs?:number|null;
-  elgLeftGeometryMs?:number|null; elgRightGeometryMs?:number|null;
-  elgLeftCanvasDrawMs?:number|null; elgRightCanvasDrawMs?:number|null;
-  elgLeftPixelReadMs?:number|null; elgRightPixelReadMs?:number|null;
-  elgLeftGrayHistogramMs?:number|null; elgRightGrayHistogramMs?:number|null;
-  elgLeftCdfMs?:number|null; elgRightCdfMs?:number|null;
-  elgLeftEqualizeNormalizeMs?:number|null; elgRightEqualizeNormalizeMs?:number|null;
-  elgInputBufferAcquireMs?:number|null;
+  gazeXRaw:number|null; gazeYRaw:number|null; gazeX:number|null; gazeY:number|null; gazeConfidence:number|null; gazeOutlier:boolean|null; gazeRawDeviationPx:number|null;
+  pupilLeft:number|null; pupilRight:number|null; headX:number|null; headY:number|null; headZ:number|null; headYaw:number|null; headPitch:number|null; headRoll:number|null; headPoseDistanceFromCalibration:number|null; eyeConfidence:number|null;
+  elgLeftRelX:number|null; elgLeftRelY:number|null; elgRightRelX:number|null; elgRightRelY:number|null; elgLeftConfidence:number|null; elgRightConfidence:number|null; elgLeftRelXRaw:number|null; elgLeftRelYRaw:number|null; elgRightRelXRaw:number|null; elgRightRelYRaw:number|null; elgBinocularReliability:number|null;
+  trial:string|null; event:string|null; calibrationId:string|null; calibrationPoints:number|null; calibrationRepetitions:number|null; calibrationSettleMs:number|null; calibrationSampleMs:number|null; calibrationRandomized:boolean|null; calibrationObservations:number|null;
+  validationMeanPx:number|null; validationMedianPx:number|null; validationRmsePx:number|null; validationPrecisionRmsS2SPx:number|null; validationPrecisionSdPx:number|null; validationDataLoss:number|null; validationValidPoints:number|null;
+  calibrationHeadReferenceSamples:number|null; calibrationHeadX:number|null; calibrationHeadXSd:number|null; calibrationHeadY:number|null; calibrationHeadYSd:number|null; calibrationHeadZ:number|null; calibrationHeadZSd:number|null; calibrationHeadYaw:number|null; calibrationHeadYawSd:number|null; calibrationHeadPitch:number|null; calibrationHeadPitchSd:number|null; calibrationHeadRoll:number|null; calibrationHeadRollSd:number|null;
+  cameraTrackFps?:number|null; videoCallbackIntervalMs?:number|null; videoPresentedFrameDelta?:number|null; videoMissedPresentedFramesTotal?:number; pipelineTotalMs?:number|null; mediaPipeDispatchToBitmapMs?:number|null; mediaPipeBitmapToPostMs?:number|null; mediaPipeWorkerQueueMs?:number|null; mediaPipeWorkerInferenceMs?:number|null; mediaPipeWorkerReturnMs?:number|null; mediaPipeEndToEndMs?:number|null; mediaPipeResultAgeMs?:number|null; mediaPipeSequenceId?:number|null;
+  elgEnqueueMs?:number|null; featureExtractionMs?:number|null; gazePredictionMs?:number|null; overlayRenderMs?:number|null; elgAcquisitionMode?:string|null; elgInferenceMs?:number|null; elgAgeMs?:number|null; elgSequenceId?:number|null; elgAcquisitionTimestampMs?:number|null; elgProcessedTimestampMs?:number|null; elgLatencyMs?:number|null; elgQueueDepth?:number|null; elgCropPreprocessMs?:number|null; elgQueueWaitMs?:number|null; elgTensorSetupMs?:number|null; elgOnnxInferenceMs?:number|null; elgDecodeMs?:number|null; elgFirstEye?:"left"|"right"|null;
+  elgLeftCropTotalMs?:number|null; elgRightCropTotalMs?:number|null; elgLeftGeometryMs?:number|null; elgRightGeometryMs?:number|null; elgLeftCanvasDrawMs?:number|null; elgRightCanvasDrawMs?:number|null; elgLeftPixelReadMs?:number|null; elgRightPixelReadMs?:number|null; elgLeftGrayHistogramMs?:number|null; elgRightGrayHistogramMs?:number|null; elgLeftCdfMs?:number|null; elgRightCdfMs?:number|null; elgLeftEqualizeNormalizeMs?:number|null; elgRightEqualizeNormalizeMs?:number|null; elgInputBufferAcquireMs?:number|null;
+  elgSharedFrameCaptureMs?:number|null; elgSharedLeftCropMs?:number|null; elgSharedRightCropMs?:number|null; elgSharedLeftMeanAbsDiff?:number|null; elgSharedRightMeanAbsDiff?:number|null; elgSharedLeftMaxAbsDiff?:number|null; elgSharedRightMaxAbsDiff?:number|null;
 }
